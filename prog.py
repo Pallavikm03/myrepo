c3f44267f14b3fa965051d9d0bbe830e913edb74
Pallavikm03 <mpallavi@zeomega.com>
2014-04-10 14:42:10 +0530

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-def sorting():
-""" doc for sorting a list"""
-	list1=[3,5,7,8,1]
-	for i in sorted(list1):
-		print i 
-
-
-"""calling procedure"""
-=======
 def example():
 	"""Example for filter and lambda expression"""
 	exp = filter(lambda x: x % 3 == 0, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 	print exp
 	
->>>>>>> 0373d417ceca560f79ad6e53d1604ada7b84409f
 if __name__=="__main__":
 	example()
 print 'Hello world !'
