685c7a47719fe60e9eeec93391a3910a7fe5c3e9
SAFIULLA <jsafiulla@zeomega.com>
2014-04-10 13:38:45 +0530

    change made by safi

--- prog.py
+++ prog.py
@@ -1,14 +1,7 @@
-def sorting():
-""" doc for sorting a list"""
-	list1=[3,5,7,8,1]
-	for i in sorted(list1):
-		print i 
-
-
-
+def emxaple():
+"""Example for filter and lambda expression"""
+	exp = filter(lambda x: x % 3 == 0, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+	return exp
+	
 if __name__=="__main__":
-	sorting()
-print"vidhya"
-
-
-what you did???
+	example()
