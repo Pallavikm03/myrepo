0a71b3626e1cc61fadc12ea1ff86a9a11e38fa4b
SAFIULLA <jsafiulla@zeomega.com>
2014-04-10 14:16:59 +0530

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,7 +1,7 @@
 def example():
 	"""Example for filter and lambda expression"""
 	exp = filter(lambda x: x % 3 == 0, [1, 2, 3, 4, 5, 6, 7, 8, 9])
-	return exp
+	print exp
 	
 if __name__=="__main__":
 	example()
