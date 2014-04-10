12d79ae0c237692f3459781ac2b0c99eb60cc64b
SAFIULLA <jsafiulla@zeomega.com>
2014-04-10 14:05:31 +0530

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,5 +1,5 @@
 def emxaple():
-"""Example for filter and lambda expression"""
+	"""Example for filter and lambda expression"""
 	exp = filter(lambda x: x % 3 == 0, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 	return exp
 	
